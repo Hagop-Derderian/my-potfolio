154c1244c0047c89f21ac0c72a0e7523c0d97c15
Hagop-Derderian <hagop_111@outlook.com>
2022-12-07 12:24:52 +0400

    added audio

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,18 @@ import RouterLayout from "./Router/RouterLayout";
 import RouterLayoutError from "./Router/RouterLayoutError";
 import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
+import { useRef } from "react";
 import { useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { GiSoundOff, GiSoundOn } from "react-icons/gi";
 // import ReactAudioPlayer from "react-audio-player";
-// import detroitMp3 from "./Detroit-Convention-Center-Ambient.mp3";
-import silence from "./250-milliseconds-of-silence.mp3";
+import BgThemeSong from "./Detroit-Convention-Center-Ambient.mp3";
+// import ReactAudioPlayer from "react-audio-player";
 function App() {
   let [colorId, setColorId] = useState("red");
-
+  const [sound, setSound] = useState(false);
+  const musicRef = useRef(null);
   useEffect(() => {
     let color = localStorage.getItem("Color");
     AOS.init();
@@ -46,15 +49,31 @@ function App() {
       </Routes>
       <Footer />
       <div className="Head-div">
-        <iframe
-          src={silence}
-          allow="autoplay"
-          id="audio"
-          style={{ display: "none" }}
-        ></iframe>
-        <audio id="player" autoplay loop>
-          <source src="audio/source.mp3" type="audio/mp3"></source>
-        </audio>
+        <audio
+          className=""
+          src={BgThemeSong}
+          controls={false}
+          type={"audio/mp3"}
+          loop
+          ref={musicRef}
+        ></audio>
+        <button
+          className="SoundBtn"
+          onClick={() => {
+            musicRef.current.volume = 0.3;
+            setSound(!sound);
+            if (sound === true) {
+              musicRef.current.pause();
+            } else musicRef.current.play();
+          }}
+        >
+          {sound === false ? (
+            <GiSoundOff className="Sound-icon" />
+          ) : (
+            <GiSoundOn className="Sound-icon" />
+          )}
+        </button>
+
         <div className="frame-right"></div>
         <div className="frame-left"></div>
         <div className="frame-top"></div>
